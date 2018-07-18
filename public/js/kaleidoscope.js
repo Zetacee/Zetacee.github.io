c1b5becc67321b19da63a901fe7d527b29cae6eb
Sergio Zaragoza <sergiozcontacto@gmail.com>
2018-07-19 01:11:31 +0200

    Click, mandala y texto

--- public/js/kaleidoscope.js
+++ public/js/kaleidoscope.js
@@ -116,3 +116,57 @@ document.querySelector('#Mandala').classList.add('view')
 //Esta clase lo que hace es añadir una lista una al identificador "Mandala",
 // que pertenece a 'canvas' (llamado como un lienzo que se edita con JS)
 //el nombre de la nueva clase que añadimos a este ID es 'view'. El cual se accede en css.
+
+
+
+
+//***********************************************************************
+// JS, de las letras moviendose
+var area = document.getElementById('area'),
+	list = [
+		'designer',
+		'photographer',
+		'programmer',
+		'engineer',
+		'unstoppable',
+    'Sergio Zaragoza'
+	],
+	count_li = 0,
+	count = 0,
+	speed = 200;
+
+
+function rewrite() {
+	var type = list[count_li].substring(0, count);
+	document.getElementById('area').textContent = type;
+	count++;
+	var timer = setTimeout(rewrite, speed);
+	if(count > list[count_li].length) {
+		count = 0;
+		count_li++;
+		clearTimeout(timer);
+		setTimeout(rewrite, 2500);
+	}
+
+	if(count_li === list.length) {
+		count_li = 0;
+	}
+}
+
+rewrite();
+
+
+var flag = true;
+
+function flashing() {
+	if(flag) {
+		document.getElementById('cursor').style.opacity = 1;
+	} else {
+		document.getElementById('cursor').style.opacity = 0;
+	}
+	flag = !flag
+	setTimeout(flashing, 500);
+}
+
+flashing();
+//***********************************************************************
